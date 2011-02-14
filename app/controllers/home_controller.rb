225b590e0b54e8547c78393b09a831c001798fc4
ivobenedito <ivobenedito@gmail.com>
2011-02-14 02:02:05 +0000

    done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,10 +11,10 @@ class HomeController < ApplicationController
   	
   	ttickers.each do |ticker|
   		params = {:s => ticker.code, :f => ticker.format}	
-  		fullname1, name, index, date, time, change, pchange = Net::HTTP.post_form(app_uri, params).body.chomp.split(',')
-  		ticker_response = { :id => ticker.id, :fullname1 => fullname1.gsub('"',''),
-  			:name => name.gsub('"',''), :index => index.gsub('"',''), :date => date.gsub('"',''),
-  			:time => time.gsub('"',''), :change => change.gsub('"',''), :pchange => pchange.gsub('"','') }
+  		code, name, date, time, value, change, pchange, volume = Net::HTTP.post_form(app_uri, params).body.chomp.split(',')
+  		ticker_response = { :id => ticker.id, :code => code.gsub('"',''),
+  			:name => name.gsub('"',''), :date => date.gsub('"',''), :time => time.gsub('"',''), :value => value.gsub('"',''), 
+  			:change => change.gsub('"',''), :pchange => pchange.gsub('"',''), :volume => volume.gsub('"','') }
   		@tickers.push(ticker_response)
   	end
   end
